86b115b74eba842543df01722ccc8c0be7a30425
ipeinado <i.peinado.martinez@gmail.com>
2020-05-19 23:34:45 +0200

    added i18next for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, APP_INITIALIZER, LOCALE_ID } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { I18NextModule, ITranslationService, I18NEXT_SERVICE, I18NextTitle, defaultInterpolationFormat } from 'angular-i18next';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import XHR from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+export function appInit(i18next: ITranslationService) {
+  return () => 
+    i18next
+      .use(XHR)
+      .use(LanguageDetector)
+      .init({
+        whitelist: ['en', 'es', 'da', 'pt'],
+        fallbackLng: 'en',
+        debug: true,
+        returnEmptyString: false,
+        ns: [
+          'translation',
+          'validation',
+          'error'
+        ],
+        interpolation: {
+          format: I18NextModule.interpolationFormat(defaultInterpolationFormat)
+        },
+        backend: {
+          loadPath: 'assets/locales/{{lng}}.{{ns}}.json'
+        },
+        detection: {
+          order: ['querystring', 'cookie'],
+          lookupCookie: 'lang',
+          lookupQuerystring: 'lng',
+          caches: ['localStorage', 'cookie'],
+          cookieMinutes: 10080,
+        },
+        
+
+      });
+}
+
+export function localeIdFactory(i18next: ITranslationService) {
+  return i18next.language;
+}
+
+export const I18N_PROVIDERS = [
+  {
+    provide: APP_INITIALIZER,
+    useFactory: appInit,
+    deps: [I18NEXT_SERVICE],
+    multi: true
+  },
+  {
+    provide: LOCALE_ID,
+    deps: [I18NEXT_SERVICE],
+    useFactory: localeIdFactory
+  }
+];
 
 @NgModule({
   declarations: [
@@ -15,8 +70,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    I18NextModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    I18N_PROVIDERS
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
